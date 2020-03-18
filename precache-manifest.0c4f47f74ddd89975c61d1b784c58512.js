6b1fd7a877baf19272a0d91bd8185813ff585429
carlrich <csama.jr@gmail.com>
2020-03-18 17:46:06 +0100

    Updates

---
+++ precache-manifest.0c4f47f74ddd89975c61d1b784c58512.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8063330f40b18387f0d3e6d8e334f538",
+    "url": "/PUG-PETS/index.html"
+  },
+  {
+    "revision": "1e27377c15a3df3295e5",
+    "url": "/PUG-PETS/static/css/main.3ab75217.chunk.css"
+  },
+  {
+    "revision": "9ead24397a1e001645b3",
+    "url": "/PUG-PETS/static/js/2.16c8125b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/PUG-PETS/static/js/2.16c8125b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e27377c15a3df3295e5",
+    "url": "/PUG-PETS/static/js/main.202d79dc.chunk.js"
+  },
+  {
+    "revision": "e7e064f932bc877de00e",
+    "url": "/PUG-PETS/static/js/runtime-main.e760a735.js"
+  },
+  {
+    "revision": "08a4a847454401d997f3a0ce8aaf237c",
+    "url": "/PUG-PETS/static/media/akashpug.08a4a847.jpg"
+  },
+  {
+    "revision": "96b58eed284b72985c6f6257bb55bad3",
+    "url": "/PUG-PETS/static/media/coverpic.96b58eed.jpg"
+  },
+  {
+    "revision": "d2f2e4695e4d0b13cc70f3c1923451a7",
+    "url": "/PUG-PETS/static/media/coverpicture.d2f2e469.jpg"
+  },
+  {
+    "revision": "7e97e390e79ebb1a41e677471fcaa518",
+    "url": "/PUG-PETS/static/media/kc-pug.7e97e390.jpg"
+  },
+  {
+    "revision": "6a118acd38d9bdfe209cc94804d63fc8",
+    "url": "/PUG-PETS/static/media/kc-pug1.6a118acd.jpg"
+  },
+  {
+    "revision": "07ca692d0dda41f78b6611773fd00bf9",
+    "url": "/PUG-PETS/static/media/kc-pug2.07ca692d.jpg"
+  },
+  {
+    "revision": "124c3e595332b0f5c44586873e094343",
+    "url": "/PUG-PETS/static/media/kc-pug3.124c3e59.jpg"
+  },
+  {
+    "revision": "11adf96f3c906e015366cd1d2dc2c5d1",
+    "url": "/PUG-PETS/static/media/kc-pug4.11adf96f.png"
+  },
+  {
+    "revision": "203ccd0540dc63352057cfca7d7bfbfe",
+    "url": "/PUG-PETS/static/media/kc-pug5.203ccd05.jpg"
+  },
+  {
+    "revision": "5f719fc85131f1c9621e76e0b8173fd2",
+    "url": "/PUG-PETS/static/media/kc-pug6.5f719fc8.jpg"
+  },
+  {
+    "revision": "7c1cbcc324f1e9a51e38940773bb305c",
+    "url": "/PUG-PETS/static/media/snapitc.7c1cbcc3.ttf"
+  }
+]);
